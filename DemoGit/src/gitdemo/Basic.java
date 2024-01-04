3b6b558fb9e0f27facc37cf1a6c45f35fe08349d
iamAmit681 <iamAmit681@gmail.com>
2024-01-04 14:50:48 +0530

    "Changes done in same Class US-004"

--- DemoGit/src/gitdemo/Basic.java
+++ DemoGit/src/gitdemo/Basic.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class Basic {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Welcome");
 		System.out.println("i");
 		System.out.println("am");
 		System.out.println("Amit");
